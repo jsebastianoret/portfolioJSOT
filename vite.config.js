0e45a56e3f815fee6b053b7d40048fc597c57ae2
jsebastianoret <118024431+jsebastianoret@users.noreply.github.com>
2024-05-01 16:49:35 -0500

    5

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jsebastianoret.github.io/portfolioJSOT',
+  base: '/portfolioJSOT/'
   
 })
